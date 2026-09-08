fd50b0e4da9b45cd9796e123aa1d7fcb3c999975
DESKTOP-DNS42D8\maxbo <maxbobercki1@mail.ru>
2026-09-08 11:58:33 +0300

    задания по циклам

--- 1/1/1.1.cpp
+++
@@ -1,8 +0,0 @@
-﻿#ifndef INOUT_H
-#define INOUT_H
-
-#include <vector>
-void print(const std::vector<int>& vec);
-void read(std::vector<int>& vec);
-
-#endif // INOUT_H
--- dz1/dz1/Program.cs
+++ dz1/dz1/Program.cs
@@ -1,28 +1,93 @@
-﻿//№1
-using System;
+﻿////№1
+//using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.Write("Введите первое число: ");
-        int biba = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Введите второе число: ");
-        int boba = Convert.ToInt32(Console.ReadLine());
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.Write("Введите сумму вклада: ");
+//        decimal sum = Convert.ToDecimal(Console.ReadLine());
+//        Console.Write("Введите количество месяцев: ");
+//        int months = Convert.ToInt32(Console.ReadLine());
 
-        if (biba == boba)
-        {
-            Console.WriteLine("Два числа равны");
-        }
-        else if (biba > boba)
-        {
-            Console.WriteLine("Первое число больше второго");
-        }
-        else
-        {
-            Console.WriteLine("Первое число меньше второго");
-        }
-    }
-}
+//        for (int i = 0; i < months; i++)
+//        {
+//            sum = sum + sum * 0.07m;
+//        }
+
+//        Console.WriteLine("Конечная сумма: " + sum);
+//    }
+//}
 
 
+////№2
+//using System;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.Write("Введите сумму вклада: ");
+//        decimal sum = Convert.ToDecimal(Console.ReadLine());
+//        Console.Write("Введите количество месяцев: ");
+//        int months = Convert.ToInt32(Console.ReadLine());
+
+//        int i = 0;
+//        while (i < months)
+//        {
+//            sum = sum + sum * 0.07m;
+//            i++;
+//        }
+
+//        Console.WriteLine("Конечная сумма: " + sum);
+//    }
+//}
+
+
+
+////№3
+//using System;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        for (int i = 1; i <= 10; i++)
+//        {
+//            for (int j = 1; j <= 10; j++)
+//            {
+//                Console.Write(i * j + "\t");
+//            }
+//            Console.WriteLine();
+//        }
+//    }
+//}
+
+
+
+////№4
+//using System;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        while (true)
+//        {
+//            Console.Write("Введите первое число: ");
+//            int biba = Convert.ToInt32(Console.ReadLine());
+//            Console.Write("Введите второе число: ");
+//            int boba = Convert.ToInt32(Console.ReadLine());
+
+//            if (biba >= 0 && biba <= 10 && boba >= 0 && boba <= 10)
+//            {
+//                Console.WriteLine("Результат: " + (biba * boba));
+//                break;
+//            }
+//            else
+//            {
+//                Console.WriteLine("Введенные числа недопустимы");
+//            }
+//        }
+//    }
+//}
